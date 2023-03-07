cd9ff029be7f7c8fabea16f7cb24c52924af5690
micahglausier <122926346+micahglausier@users.noreply.github.com>
2023-03-07 14:29:43 -0600

    Added CORS and added allowed Origins

--- index.js
+++ index.js
@@ -30,6 +30,19 @@ mongoose.set('strictQuery', true);
 //Integrating Mongoose with RESTAPI cfDB is the name of Database with movies and users
 mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //importing auth.js
 let auth = require('./auth')(app);
