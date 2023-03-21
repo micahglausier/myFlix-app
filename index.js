b498a775d147f7b1e8dfe63f947b271304a9a2cc
Micah Glausier <micah.glausier@me.com>
2023-03-20 22:16:22 -0500

    updated CORS

--- index.js
+++ index.js
@@ -34,18 +34,18 @@ mongoose.set('strictQuery', true);
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234/'];
+// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234/'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
- }));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+//  }));
 
 //importing auth.js
 let auth = require('./auth')(app);
