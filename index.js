c322d95f420f5d13ea35327e76a6ce5637ba4726
Micah Glausier <micah.glausier@me.com>
2023-03-21 14:09:58 -0500

    Updated CORS

--- index.js
+++ index.js
@@ -16,20 +16,18 @@ const corsOptions ={
 };
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234/', 'https://myflix-micah.herokuapp.com/'];
 
-// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234/'];
-
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//       return callback(new Error(message ), false);
-//     }
-//     return callback(null, true);
-//   }
-//  }));
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+ }));
 
 
 const { check, validationResult } = require('express-validator');
